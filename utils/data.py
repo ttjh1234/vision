d25897331391615c340b12d7b887675f3d187a7a
ttjh1234 <ttjh1234@naver.com>
2023-01-19 17:18:16 +0900

    Slight Change

--- utils/data.py
+++ utils/data.py
@@ -4,41 +4,66 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 from collections import defaultdict as ddict
-import matplotlib as mpl
 from torch.utils.data import TensorDataset, DataLoader,Dataset, random_split
 import torchvision
 import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
 
-def data_loader_cifar10(path=None, seed=42):
+def data_loader_cifar10(path=None):
     # Cifar10 Data Fetch & Preprocessing & Split 
 
     transform = transforms.Compose([transforms.RandomCrop(32,4),transforms.RandomHorizontalFlip(),transforms.ToTensor(),transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
     transform2 = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
     if path:
-        trainset = torchvision.datasets.CIFAR10(path+'/train',train=True,download=False,transform=transform)
+        train_set = torchvision.datasets.CIFAR10(path+'/train',train=True,download=False,transform=transform)
         test_set = torchvision.datasets.CIFAR10(path+'/test',train=False,download=False,transform=transform2)
 
     else:
-        trainset = torchvision.datasets.CIFAR10("../assets/data/cifar10/train",train=True,download=False,transform=transform)
+        train_set = torchvision.datasets.CIFAR10("../assets/data/cifar10/train",train=True,download=False,transform=transform)
         test_set = torchvision.datasets.CIFAR10("../assets/data/cifar10/test",train=False,download=False,transform=transform2)
-    train_dataset, valid_dataset= random_split(trainset, [45000, 5000],generator=torch.Generator().manual_seed(seed))
-
-    # label check
-    label_dict=ddict(int)
-    for i in train_dataset:
-        label_dict[i[1]]+=1
-        
+            
     # 0 ~ 9 label exists,
     # Some detail, 
     classes = ['plane', 'car', 'bird', 'cat',
             'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-    train_loader=DataLoader(train_dataset,batch_size=128,shuffle=True)
-    valid_loader=DataLoader(valid_dataset,batch_size=128,shuffle=True)
+    train_loader=DataLoader(train_set,batch_size=128,shuffle=True)
     test_loader=DataLoader(test_set,batch_size=128,shuffle=False)
 
-    return train_loader,valid_loader,test_loader, classes
+    return train_loader, test_loader, classes
+
+def fetch_subset_cifar10(path=None,subset_percent=1,seed=42):
+    # Cifar10 Data Fetch & Preprocessing & Split 
+
+    transform = transforms.Compose([transforms.RandomCrop(32,4),transforms.RandomHorizontalFlip(),transforms.ToTensor(),transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+    transform2 = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+
+    if path:
+        train_set = torchvision.datasets.CIFAR10(path+'/train',train=True,download=False,transform=transform)
+        test_set = torchvision.datasets.CIFAR10(path+'/test',train=False,download=False,transform=transform2)
+
+    else:
+        train_set = torchvision.datasets.CIFAR10("../assets/data/cifar10/train",train=True,download=False,transform=transform)
+        test_set = torchvision.datasets.CIFAR10("../assets/data/cifar10/test",train=False,download=False,transform=transform2)
+            
+    # 0 ~ 9 label exists,
+    # Some detail, 
+    classes = ['plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    
+    trainset_size = len(train_set)
+    indices = list(range(trainset_size))
+    split = int(np.floor(subset_percent * trainset_size))
+    np.random.seed(seed)
+    np.random.shuffle(indices)
+
+    train_sampler = SubsetRandomSampler(indices[:split])
+    
+    train_loader=DataLoader(train_set,batch_size=128,sampler=train_sampler)
+    test_loader=DataLoader(test_set,batch_size=128,shuffle=False)
+
+    return train_loader, test_loader, classes
 
 
 
@@ -86,49 +111,16 @@ def data_loader_mnist(path=None):
 
     return train_loader,valid_loader, test_loader, classes
 
-def mnist_ig_data_loader(path=None):
-        
-    ig_scale_train=np.load('./assets/ig/ig_scale_train.npy')
-    ig_scale_valid=np.load('./assets/ig/ig_scale_valid.npy')
-    ig_scale_test=np.load('./assets/ig/ig_scale_test.npy')
-
-    ig_label_train=np.load('./assets/ig/ig_label_train.npy')
-    ig_label_valid=np.load('./assets/ig/ig_label_valid.npy')
-    ig_label_test=np.load('./assets/ig/ig_label_test.npy')
-
-    ig_scale_train=ig_scale_train.transpose([0,3,1,2])
-    ig_scale_valid=ig_scale_valid.transpose([0,3,1,2])
-    ig_scale_test=ig_scale_test.transpose([0,3,1,2])
-
-    ig_scale_train=torch.FloatTensor(ig_scale_train)
-    ig_label_train=torch.LongTensor(ig_label_train).squeeze(1)
-    train_ds = TensorDataset(ig_scale_train,ig_label_train)
-
-    ig_scale_valid=torch.FloatTensor(ig_scale_valid)
-    ig_label_valid=torch.LongTensor(ig_label_valid).squeeze(1)
-    valid_ds = TensorDataset(ig_scale_valid,ig_label_valid)
-
-    ig_scale_test=torch.FloatTensor(ig_scale_test)
-    ig_label_test=torch.LongTensor(ig_label_test).squeeze(1)
-    test_ds = TensorDataset(ig_scale_test,ig_label_test)
-
-    train_loader=DataLoader(train_ds,batch_size=128,shuffle=True)
-    valid_loader=DataLoader(valid_ds,batch_size=128,shuffle=True)
-    test_loader=DataLoader(test_ds,batch_size=128,shuffle=False)
-
-    return train_loader, valid_loader, test_loader
-
-
 def cifar10_experiment_load(path=None):
     classes = ['plane', 'car', 'bird', 'cat',
             'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
     if path==None:
-        train_data=np.load('./assets/data/experiment/cifar10_train_data.npy')
-        train_label=np.load('./assets/data/experiment/cifar10_train_label.npy')
-        valid_data=np.load('./assets/data/experiment/cifar10_valid_data.npy')
-        valid_label=np.load('./assets/data/experiment/cifar10_valid_label.npy')
-        test_data=np.load('./assets/data/experiment/cifar10_test_data.npy')
-        test_label=np.load('./assets/data/experiment/cifar10_test_label.npy')
+        train_data=np.load('../assets/data/experiment/cifar10_train_data.npy')
+        train_label=np.load('../assets/data/experiment/cifar10_train_label.npy')
+        valid_data=np.load('../assets/data/experiment/cifar10_valid_data.npy')
+        valid_label=np.load('../assets/data/experiment/cifar10_valid_label.npy')
+        test_data=np.load('../assets/data/experiment/cifar10_test_data.npy')
+        test_label=np.load('../assets/data/experiment/cifar10_test_label.npy')
     
     else:
         train_data=np.load(path+'/cifar10_train_data.npy')
